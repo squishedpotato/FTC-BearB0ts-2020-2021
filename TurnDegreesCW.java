be5d182e52c61c4349542af8f63319269aba701b
squishedpotato <76015998+squishedpotato@users.noreply.github.com>
2021-10-20 16:03:16 -0400

    Update TurnDegreesCW.java

--- TurnDegreesCW.java
+++ TurnDegreesCW.java
@@ -1,3 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
 public class TurnDegreesCW(double degrees,double power){
   frontleft.setTargetPosition((int)(-2400.0/90*degrees));
   backright.setTargetPosition((int)(2400.0/90*degrees));
