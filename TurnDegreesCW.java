384bc58aadd3f68a6730a869be3d21bcf14e8b35
squishedpotato <76015998+squishedpotato@users.noreply.github.com>
2021-11-09 16:01:42 -0500

    Update TurnDegreesCW.java

--- TurnDegreesCW.java
+++ TurnDegreesCW.java
@@ -17,21 +17,49 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 public class TurnDegreesCW extends HardwareMap{
-  private double degrees;
-  private double power;
-  private DcMotor motorFrontLeft = hardwareMap.get(DcMotor.class,"");
-  private DcMotor motorBackRight = hardwareMap.get(DcMotor.class,"");
-  private DcMotor motorFrontRight = hardwareMap.get(DcMotor.class,"");
-  private DcMotor motorBackLeft = hardwareMap.get(DcMotor.class,"");
-  public TurnDegreesCW(double degreesJoystick,double powerMotor, DcMotor m1, DcMotor m2, DcMotor m3, DcMotor m4){
-    degrees = degreesJoystick;
-    power = powerMotor;
+  private String frontLeftN;
+  private String backRightN;
+  private String frontRightN;
+  private String backLeftN;
+  private DcMotor motorFrontLeft = hardwareMap.get(DcMotor.class,n1);
+  private DcMotor motorBackRight = hardwareMap.get(DcMotor.class,n2);
+  private DcMotor motorFrontRight = hardwareMap.get(DcMotor.class,n3);
+  private DcMotor motorBackLeft = hardwareMap.get(DcMotor.class,n4);
+  public TurnDegreesCW(DcMotor m1, String n1, DcMotor m2, String n2, DcMotor m3, String n3, DcMotor m4, String n4){
     motorFrontLeft = m1;
     motorBackRight = m2;
     motorFrontRight = m3;
     motorBackLeft = m4;
+    frontLeftN = n1;
+    backRightN = n2;
+    frontRightN = n3;
+    backLeftN = n4;
   }
-  
+  public void TurnCW(double degreesJoystick,double powerMotor){
+    m1.setTargetPosition((int)(-2400.0/90*degrees));
+    m2.setTargetPosition((int)(2400.0/90*degrees));
+    m3.setTargetPosition((int)(2400.0/90*degrees));
+    m4.setTargetPosition((int)(-2400.0/90*degrees));
+    m1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    m2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    m3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    m4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    m1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    m2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    m3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    m4.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    m1.setPower(powerMotor);
+    m2.setPower(powerMotor);
+    m3.setPower(powerMotor);
+    m4.setPower(powerMotor);
+    while(frontleft.isBusy()){
+        telemetry.update();
+    }
+    frontleft.setPower(0);
+    frontright.setPower(0);
+    backleft.setPower(0);
+    backright.setPower(0); 
+  }
   /*m1.setTargetPosition((int)(-2400.0/90*degrees));
   m2.setTargetPosition((int)(2400.0/90*degrees));
   m3.setTargetPosition((int)(2400.0/90*degrees));
